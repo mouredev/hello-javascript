44d3d43cd344575b5c7d8f8eac9ed20dea45bf2b
MarcosLombardo <marcoslombardo28@gmail.com>
2024-07-20 15:58:42 -0300

    Prueba de GitHub

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -6,7 +6,16 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 
 // 1. Escribe un comentario en una línea
 
+// Comentario de una linea
+
 // 2. Escribe un comentario en varias líneas
+
+/* 
+Comentario
+de
+varias
+lineas
+*/
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 
