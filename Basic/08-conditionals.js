60cf221bc9be62686bc27de4b4bfd28978ad277d
Brais Moure <mouredev@gmail.com>
2024-07-25 07:48:50 +0200

    Clase 3 | 24/07/2024
    Condicionales, arrays y sets

---
+++ Basic/08-conditionals.js
@@ -0,0 +1,71 @@
+/*
+Clase 3 en vídeo | 24/07/2024
+Condicionales, arrays y sets
+https://www.twitch.tv/videos/2206228701?t=00h16m02s
+*/
+
+// if, else if, else
+
+// if (si)
+
+let age = 37
+
+if (age == 37) {
+    console.log("La edad es 37")
+}
+
+// else (si no)
+
+if (age == 37) {
+    console.log("La edad es 37")
+} else {
+    console.log("La edad no es 37")
+}
+
+// else if (si no, si)
+
+if (age == 37) {
+    console.log("La edad es 37")
+} else if (age < 18) {
+    console.log("Es menor de edad")
+} else {
+    console.log("La edad no es 37 ni es menor de edad")
+}
+
+// Operador ternario
+
+const message = age == 37 ? "La edad es 37" : "La edad no es 37"
+console.log(message)
+
+// switch
+
+let day = 3
+let dayName
+
+switch (day) {
+    case 0:
+        dayName = "Lunes"
+        break
+    case 1:
+        dayName = "Martes"
+        break
+    case 2:
+        dayName = "Miércoles"
+        break
+    case 3:
+        dayName = "Jueves"
+        break
+    case 4:
+        dayName = "Viernes"
+        break
+    case 5:
+        dayName = "Sábado"
+        break
+    case 6:
+        dayName = "Domingo"
+        break
+    default:
+        dayName = "Número de día incorrecto"
+}
+
+console.log(dayName)
