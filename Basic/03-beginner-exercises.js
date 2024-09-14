41a27a2a1e92b5291d0d3ec1037e7ea3e27977a5
Yoandy Doble Herrera <yoandy.doble@gmail.com>
2024-09-14 12:44:28 -0400

    Excercises entry level javascript

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -2,13 +2,43 @@
 Clase 2 en vídeo | 17/07/2024
 Tipos de datos, operadores y strings
 https://www.youtube.com/live/euVbF1eatEY?si=nHS3QznAlA_C39NK&t=808
+
+Ejercicios Yoandy Doble Herrera
+Author: codebydoble
 */
 
 // 1. Escribe un comentario en una línea
+//Este es el comentario en una sola línea by codebydoble
 
 // 2. Escribe un comentario en varias líneas
 
+/*Este es un comentario
+que aparece en varias líneas
+de código  */
+
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+//String
+let aCadena = "This is a string";
+
+//Number
+let aNumber = 22;
+
+//Boolean
+let isHot = true;
+
+//Null
+let bonusTotal = null;
+
+//Undefined
+let countValue = undefined;
+
+//Symbol
+let symbolLetter = Symbol("mysymbol");
+
+//BigInt
+let aBigInt = BigInt(1236548799898989898989899999999999999);
+let hugeNumber = 99999999999999999999999999999999999999999n;
 
 // 4. Imprime por consola el valor de todas las variables
 
