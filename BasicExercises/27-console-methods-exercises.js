4f99fb55363b20ecc860147b713d9bc0fccfde4f
Yoandy Doble Herrera <yoandy.doble@gmail.com>
2024-10-21 19:09:02 -0400

    Initial excercises Level I

---
+++ BasicExercises/27-console-methods-exercises.js
@@ -0,0 +1,25 @@
+/*
+Clase 7 en vídeo | 21/08/2024
+Console y módulos
+https://www.youtube.com/live/PAnxhBE5kIE?si=V0F_NsKO9lmhhatu&t=555
+*/
+
+// 1. Crea un función que utilice error correctamente
+
+// 2. Crea una función que utilice warn correctamente
+
+// 3. Crea una función que utilice info correctamente
+
+// 4. Utiliza table
+
+// 5. Utiliza group
+
+// 6. Utiliza time
+
+// 7. Valida con assert si un número es positivo
+
+// 8. Utiliza count
+
+// 9. Utiliza trace
+
+// 10. Utiliza clear
