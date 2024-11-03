ee540c92c86921d72d07641e92680b8fe3a1a3d4
Yoandy Doble Herrera <yoandy.doble@gmail.com>
2024-11-02 21:04:09 -0400

    Function excersices array

--- BasicExercises/17-functions-exercises.js
+++ BasicExercises/17-functions-exercises.js
@@ -9,8 +9,8 @@ https://www.youtube.com/live/xg1GeHKiNzU?si=oiWHK8bDTZ1c98hF&t=885
 // 1. Crea una función que reciba dos números y devuelva su suma
 
 const sumNumbers = (number1, number2) => {
-  /* Declarative function */
-  return number1 + number2
+    /* Declarative function */
+    return number1 + number2
 }
 
 console.log(sumNumbers(100, 36))
@@ -18,13 +18,13 @@ console.log(sumNumbers(100, 36))
 // 2. Crea una función que reciba un array de números y devuelva el mayor de ellos
 
 const maxNumber = (array) => {
-  let bigNumber = -Infinity
-  for (const element of array) {
-    if (bigNumber < element) {
-      bigNumber = element
+    let bigNumber = -Infinity
+    for (const element of array) {
+        if (bigNumber < element) {
+            bigNumber = element
+        }
     }
-  }
-  return bigNumber
+    return bigNumber
 }
 
 let numbersArray = [100, 35, 87, 16, 777, 56, 91, 1111111, 37, -5, 34, -8973]
@@ -34,61 +34,60 @@ console.log(maxNumber(numbersArray))
 // 3. Crea una función que reciba un string y devuelva el número de vocales que contiene
 
 const vocalTotal = (strValue) => {
-  /*Case I*/
-  let vocalArray = ["a", "e", "i", "o", "u"]
-  let count = 0
+    /*Case I*/
+    let vocalArray = ["a", "e", "i", "o", "u"]
+    let count = 0
 
-  for (let index = 0; index < strValue.length; index++) {
-    if (vocalArray.includes(strValue[index])) {
-      count++
+    for (let index = 0; index < strValue.length; index++) {
+        if (vocalArray.includes(strValue[index])) {
+            count++
+        }
     }
-  }
-  return count
+    return count
 }
 
 const vocalTotal1 = (strValue) => {
-  /*Case II Switch*/
-  let otherWords = []
-  let myCountVocals = []
-  let count = 0
-  let countA = 0
-  let countE = 0
-  let countI = 0
-  let countO = 0
-  let countU = 0
-  for (let index = 0; index < strValue.length; index++) {
-    switch (strValue[index]) {
-      case "a":
-        count++
-        countA++
-        break
-      case "e":
-        count++
-        countE++
-        break
-      case "i":
-        count++
-        countI++
-        break
-      case "o":
-        count++
-        countO++
-        break
-      case "u":
-        count++
-        countU++
-        break
-      default:
-        // Letras que no son vocales
-        otherWords.push(strValue[index])
+    /*Case II Switch*/
+    let otherWords = []
+    let myCountVocals = []
+    let count = 0
+    let countA = 0
+    let countE = 0
+    let countI = 0
+    let countO = 0
+    let countU = 0
+    for (let index = 0; index < strValue.length; index++) {
+        switch (strValue[index]) {
+            case "a":
+                count++
+                countA++
+                break
+            case "e":
+                count++
+                countE++
+                break
+            case "i":
+                count++
+                countI++
+                break
+            case "o":
+                count++
+                countO++
+                break
+            case "u":
+                count++
+                countU++
+                break
+            default:
+                // Letras que no son vocales
+                otherWords.push(strValue[index])
+        }
     }
-  }
-  myCountVocals.push(countA, countE, countI, countO, countU)
-  return myCountVocals
+    myCountVocals.push(countA, countE, countI, countO, countU)
+    return myCountVocals
 }
 
-let myString =
-  "This is a very simple text made by codebydoble for Hello Javascript project."
+let myString = "This is a very simple text made by codebydoble for Hello Javascript project."
 
 console.log(`The count of vocals is: ${vocalTotal(myString)}`)
 
@@ -96,46 +95,47 @@ console.log(`The count of vocals is: ${vocalTotal(myString)}`)
 let [vocalA, vocalE, vocalI, vocalO, vocalU] = vocalTotal1(myString)
 
 let dataVocals = [
-  ["Vocal A", vocalA],
-  ["Vocal E", vocalE],
-  ["Vocal I", vocalI],
-  ["Vocal O", vocalO],
-  ["Vocal U", vocalU],
+    ["Vocal A", vocalA],
+    ["Vocal E", vocalE],
+    ["Vocal I", vocalI],
+    ["Vocal O", vocalO],
+    ["Vocal U", vocalU]
 ]
 console.table(dataVocals)
 
 // 4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas
 
 const strUpperCase = (array) => {
-  let upperCaseArray = []
-  array.forEach((element) => {
-    // to STR UP
-    upperCaseArray.push(String(element).toUpperCase())
-  })
-  return upperCaseArray
+    let upperCaseArray = []
+    array.forEach((element) => {
+        // to STR UP
+        upperCaseArray.push(String(element).toUpperCase())
+    })
+    return upperCaseArray
 }
 
 let strArray = [
-  "perro",
-  "gato",
-  "casa",
-  "coche",
-  "sol",
-  "playa",
-  "libro",
-  "agua",
-  "azul",
-  "amarillo",
-  "verde",
-  "rojo",
-  "manzana",
-  "naranja",
-  "banana",
-  "computadora",
-  "telefono",
-  "silla",
-  "mesa",
-  "puerta",
+    "perro",
+    "gato",
+    "casa",
+    "coche",
+    "sol",
+    "playa",
+    "libro",
+    "agua",
+    "azul",
+    "amarillo",
+    "verde",
+    "rojo",
+    "manzana",
+    "naranja",
+    "banana",
+    "computadora",
+    "telefono",
+    "silla",
+    "mesa",
+    "puerta",
+    "sas"
 ]
 
 console.log(strUpperCase(strArray))
@@ -143,6 +143,43 @@ console.log(strUpperCase(strArray))
 // 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario
 
 // 6. Crea una función que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
+
+/* Sin el método includes de array */
+
+const commonValues = (array1, array2) => {
+    let elementsCommon = []
+
+    for (const element of array1) {
+        /*if (array2.includes(element)) {
+      elementsCommon.push(element)
+    }*/
+        for (const values of array2) {
+            if (element === values) {
+                elementsCommon.push(element)
+            }
+        }
+    }
+    return elementsCommon
+}
+
+let wordsArray = ["manzana", "naranja", "banana", "computadora", "telefono", "silla", "mesa", "puerta"]
+
+let calles = [
+    ["sasas"],
+    "manzana",
+    "Yoandy",
+    "PlP",
+    "naranja",
+    "banana",
+    "computadora",
+    "telefono",
+    "silla",
+    "Llueve",
+    "mesa",
+    "puerta"
+]
+
+console.log(commonValues(wordsArray, calles))
 
 // 7. Crea una función que reciba un array de números y devuelva la suma de todos los números pares
 
