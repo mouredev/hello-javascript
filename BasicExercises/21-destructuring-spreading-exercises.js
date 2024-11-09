65b9382b85e329f0e73b85b50053d7dbca18f771
Yoandy Doble Herrera <yoandy.doble@gmail.com>
2024-11-09 18:40:34 -0500

    Add destructuring excercises level I solution

--- BasicExercises/21-destructuring-spreading-exercises.js
+++ BasicExercises/21-destructuring-spreading-exercises.js
@@ -4,11 +4,29 @@ Objetos, desestructuración, propagación y clases
 https://www.youtube.com/live/SBXEpAx_y_Q?si=Sbqqa2-_eGwsUkH-&t=958
 */
 
-// 1. Usa desestructuración para extraer los dos primeros elementos de un array 
+// 1. Usa desestructuración para extraer los dos primeros elementos de un array
+
+let cities = ["Havana", "Milan", "Ontario", "Brasilia", "Washington"]
+
+let [nameHv, nameMilan] = cities
+
+console.log(nameHv)
+console.log(nameMilan)
 
 // 2. Usa desestructuración en un array y asigna un valor predeterminado a una variable
 
+let dognames = ["Kazam", "Toky", "Perl", "Dog", "Koky"]
+
+let [kazamValue, tokyValue, perlValue, dogValue, kokyValue, noDogname = "Chris"] = dognames
+console.log(noDogname)
+
 // 3. Usa desestructuración para extraer dos propiedades de un objeto
+
+let kid = {name: "Nathan", age: 2, parents: ["Endry", "Arlety"]}
+
+let {name} = kid
+
+console.log(name)
 
 // 4. Usa desestructuración para extraer dos propiedades de un objeto y asígnalas
 //    a nuevas variables con nombres diferentes
