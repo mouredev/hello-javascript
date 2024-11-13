308d861ceec353d881ff07c182a47f9986b27456
Yoandy Doble Herrera <yoandy.doble@gmail.com>
2024-11-13 15:41:03 -0500

    Destructuring excersices completed

--- BasicExercises/21-destructuring-spreading-exercises.js
+++ BasicExercises/21-destructuring-spreading-exercises.js
@@ -24,21 +24,68 @@ console.log(noDogname)
 
 let kid = {name: "Nathan", age: 2, parents: ["Endry", "Arlety"]}
 
-let {name} = kid
+let {name, age} = kid
 
-console.log(name)
+console.log(name, age)
 
 // 4. Usa desestructuración para extraer dos propiedades de un objeto y asígnalas
 //    a nuevas variables con nombres diferentes
 
+let jaguar = {name: "Kitty", color: "yellow", age: 3}
+
+let {name: nombre, age: edad} = jaguar
+
+console.log("Jaguar\nName: ", nombre + "\nEdad: ", edad)
+
 // 5. Usa desestructuración para extraer dos propiedades de un objeto anidado
+
+let laptopDell = {
+    model: "Dell",
+    brand: "Inspiron 24",
+    ram: 16,
+    cpu: "Core i7",
+    motherboard: {arq: 64, type: "all", price: 400},
+    performance: {rate: 1.5, sumary: "100%"}
+}
+
+let {
+    motherboard: {arq},
+    performance: {rate: ratio}
+} = laptopDell
+
+console.log("Desestructuración de objetos anidados: \nArq ->", arq + "\nRating ->", ratio)
 
 // 6. Usa propagación para combinar dos arrays en uno nuevo
 
+/* Array no 1 */
+let passengers = ["Barbarita", "Liudmila", "Alpidio", "Nina", "Arlet", "Miguel", "Moure", "Karla", "Austin", "Jennifer"]
+/* Array no 2 */
+let passengersRow2 = ["David", "Dayma", "Daviel", "Lulu", "Noemi", "Elianys", "Ernesto", "Jhon", "Brais"]
+
+let flyPlane = [...passengers, ...passengersRow2]
+console.log(flyPlane)
+
 // 7. Usa propagación para crear una copia de un array
+let newPassengers = [...passengers]
+console.log(newPassengers)
 
 // 8. Usa propagación para combinar dos objetos en uno nuevo
+let mesa = {size: 350, color: "brown"}
+let cupHouse = {type: "simple", isEmpty: false, area: 589.36}
+
+let myHouse = {...mesa, ...cupHouse}
+console.log(myHouse)
 
 // 9. Usa propagación para crear una copia de un objeto
 
+let anotherTable = {...myHouse}
+console.log("Object copied ->", anotherTable)
+
 // 10. Combina desestructuración y propagación
+
+let numberCount = ["Uno", "Dos", "Tres", "Cuatro", "Cinco"]
+let [one, , , , five, six = 6] = numberCount
+let doorHouse = {title: "Door YDH"}
+
+let personYDH = {name: "Yoandy", year: one, ...doorHouse}
+console.log(personYDH)
